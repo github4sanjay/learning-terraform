9df369be564369f1bc36cee7e9a53114b676d798
sanjay.kumar <sanjay.kumar@endowus.com>
2022-11-20 17:49:22 +0800

    delete first ec2 instance

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-
-resource "aws_instance" "first-ec2" {
-  ami = "ami-0af2f764c580cc1f9"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hello world"
-  }
-}
