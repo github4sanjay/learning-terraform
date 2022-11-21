9a19ac063e6710dadd24cf7356fb413637d23e04
sanjay.kumar <sanjay.kumar@endowus.com>
2022-11-21 14:09:40 +0800

    remove classical link

--- modules/eks/vpc.tf
+++ modules/eks/vpc.tf
@@ -14,12 +14,6 @@ resource "aws_vpc" "main" {
   # Required for EKS. Enable/disable DNS hostnames in the VPC.
   enable_dns_hostnames = true
 
-  # Enable/disable ClassicLink for the VPC.
-  enable_classiclink = false
-
-  # Enable/disable ClassicLink DNS Support for the VPC.
-  enable_classiclink_dns_support = false
-
   # Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC.
   assign_generated_ipv6_cidr_block = false
 
